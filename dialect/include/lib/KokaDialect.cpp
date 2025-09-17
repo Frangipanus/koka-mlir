3fea8a1a0bc30e6c0f0e905c3347aaca3f625b57
Paul <pjtraphael@gmail.com>
2025-09-17 13:24:33 +0200

    cmakefiles for comilation

---
+++ dialect/include/lib/KokaDialect.cpp
@@ -0,0 +1,11 @@
+#include "mlir/IR/Dialect.h"
+#include "mlir/IR/MLIRContext.h"
+#include "KokaDialect.h.inc"
+
+using namespace mlir;
+using namespace koka;
+
+void KokaDialect::initialize()
+{
+    // No ops yet, just prepare the dialect
+}
